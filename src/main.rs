f0b2518cf9f0272c649292e277e63b45b9cb8f39
Adam <adamwmcneil@gmail.com>
2024-07-21 12:22:45 -0500

    added rocket message

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,66 @@
 #[macro_use]
 extern crate rocket;
 use rocket::{Config, State};
+use rocket_contrib::json::Json;
 
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
 
+use serde::{Deserialize, Serialize};
+
 use bevy::prelude::*;
 
 #[derive(Resource)]
-struct BevyReceiver(Arc<Mutex<Receiver<String>>>);
+struct BevyReceiver(Arc<Mutex<Receiver<Movement>>>);
 
-#[get("/")]
-fn index(transmitter: State<Sender<String>>) -> () {
-    transmitter.send("Hello".to_string()).unwrap();
-    // println!("{}", message.0.to_string());
+#[derive(Serialize, Deserialize, Debug)]
+enum Movement {
+    Right,
+    EndRight (String),
+    Left (String),
+    EndLeft (String),
+    Jump (String),
+    Dive (String),
+    Join (String),
+    Leave (String),
 }
 
-#[post("/<player>")]
-fn add_player(player: String, transmitter: State<Sender<String>>) -> String {
-    transmitter.send(player.clone()).unwrap();
-    player
+#[derive(Serialize, Deserialize, Debug)]
+struct RocketMessage {
+    player: String,
+    movement: Movement,
+}
+
+#[get("/heartbeat")]
+fn heartbeat() -> &'static str {
+    "heartbeat"
+}
+
+// #[get("/")]
+// fn index(transmitter: State<Sender<String>>) -> Result<()> {
+//     transmitter.send("Hello".to_string()).unwrap();
+//     Ok(())
+//     // println!("{}", message.0.to_string());
+// }
+
+// #[post("/new/<player>")]
+// fn add_player(player: String, transmitter: State<Sender<RocketMessage>>) -> String {
+//     transmitter.send(geunwrap();
+//     player
+// }
+
+#[post("/control", data = "<rocket_message>")]
+fn control_player(rocket_message: Json<RocketMessage>, transmitter: State<Sender<RocketMessage>>) {
+    transmitter.send(rocket_message.into_inner()).unwrap();
 }
 
 fn receive_message(receiver: Res<BevyReceiver>) {
     match receiver.0.lock() {
         Ok(receiver) => {
             if let Ok(message) = receiver.try_recv() {
-                println!("{}", message)
+                println!("{}", serde_json::to_string(&message).unwrap())
             }
         }
         Err(_) => (),
@@ -37,7 +69,9 @@ fn receive_message(receiver: Res<BevyReceiver>) {
 }
 
 fn main() {
-    let (transmitter, receiver): (Sender<String>, Receiver<String>) = mpsc::channel();
+    // let rocket_message = RocketMessage{player = }
+    println!("{}", serde_json::to_string(&Movement::Right).unwrap());
+    let (transmitter, receiver): (Sender<Movement>, Receiver<Movement>) = mpsc::channel();
 
     let rocket_thread = thread::spawn(move || {
         rocket::custom(
@@ -47,7 +81,7 @@ fn main() {
                 .unwrap(),
         )
         .manage(transmitter)
-        .mount("/api/v1", routes![index, add_player])
+        .mount("/api/v1", routes![heartbeat, control_player])
         .launch();
     });
 
